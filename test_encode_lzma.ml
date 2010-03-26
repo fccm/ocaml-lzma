ede83be5f7a99aef195202c3bc11ce552264ad3d
Blue Prawn <monnier.florent@gmail.com>
2010-03-26 20:31:50 +0000

    put the uncompressed test data in a separated file

--- test_encode_lzma.ml
+++ test_encode_lzma.ml
@@ -1,48 +1,19 @@
 open Lzma
 
+let load_file f =
+  let ic = open_in f in
+  let n = in_channel_length ic in
+  let s = String.create n in
+  really_input ic s 0 n;
+  close_in ic;
+  (s)
+
+let filename = "./lzma_test.txt"
+
 let buf_len = Sys.max_string_length
-let data = "\
-
-    ALZMAL       ALZMALZMALZ  ALZMA       ALZMA        ALZ       
-    MALZMA       MALZMALZMAL  MALZM       MALZM       ZMALZ      
-      ALZ        ZMA    LZMA    ALZM      ZMA         LZMAL      
-      MA               MAL      MALZM    ALZM        MALZMA      
-      ZM              LZM       ZMALZ    M LZ        ZMA ZMA     
-      LZ             MAL        LZMALZ  LZ AL       ALZMALZMA    
-      ALZ           LZM         AL MALZ AL MA       MALZMALZM    
-     ZMAL    ZM    MAL          MA ZMALZM  ZMA     LZMA   ALZM   
-      ZMA    LZ   LZM     LZ    ZMA ZMALZ ALZM     ALZ     ALZ   
-    MALZMALZMAL  MALZMALZMAL  MALZMA ZMA ZMALZM  LZMALZ   ZMALZM 
-    ZMALZMALZMA  ZMALZMALZMA   MALZM LZ  LZMALZ  ALZMAL   LZMALZ 
-
-      amlOCa       OCaml          OCa       OCaml       amlOCamlOCa      
-     lOCamlOC    CamlOCamlO      amlOC      mlOCa       OCamlOCamlO      
-    Caml  amlO  mlO   lOCam      OCaml        mlOC      mlO    OCa       
-    lOC    Cam OCa     mlOC     amlOCa        CamlO    OCam    ml        
-   Caml    lOCamlO              OCa lOC       lOCam    m OC    Ca        
-   lOCa    amlOCa              amlOCamlO      amlOCa  OC ml    lO        
-    mlO    OCa lO              OCamlOCam      OC mlOC ml Ca    aml       
-    Cam    mlO am       Ca    amlO   lOCa     ml CamlOC  lOC  lOCa    am 
-    lOCa  OCam OCa     mlO    OCa     mlO     Cam OCaml Caml   mlO    OC 
-     mlOCamlO   lOCamlOCam  CamlOC   OCamlO amlOCa lOC mlOCamlOCamlOCaml 
-      amlOCa     mlOCaml    lOCaml   mlOCam  CamlO am  CamlOCamlOCamlOCa 
-
-
-   ngsbinding   ndings indin   inding  indings     ngsbin ingsb   ingsbi    gsbin        ding  i 
-   gsbindingsb  dingsb nding   ndings  ndingsbin   gsbind ngsbi   ngsbin   gsbindings  ndingsbind
-   sbind   sbi   ngsb   ingsb   ings   dingsbindi   bind   sbind   sbin   gsbi  ings  nding  indi
-     ndi   bin    sbi   ngsbin  ngs    ings   ding   ndi   bindin  bin   gsbi    gsb  dings      
-     dingsbind   sbi    gsbindi gsb     gsb   ings  ndi    indings ind   sbi           ngsbi     
-     ingsbindin   ind   sbindin sbi     sbi    gsb   ing   ndingsb ndi   bin              indin  
-     ngsbindings  nd    bin ingsbin     bin   gsbi   ng    din sbindin   ind   sbindi      ding  
-     gsb    ngsb  di    ind ngsbind    bind  gsbin   gs    ing binding    di    indin       ngsb 
-     sbindingsbi ding   ndi  sbindi   bindingsbind  gsbi   ngs  ndings    ing   ndin  bind  gsbi 
-   gsbindingsbi dingsb nding  indin   indingsbind  gsbind ngsbi  ingsb     gsbindin   indingsbin 
-   sbindingsbi  ingsbindings   ding   ndingsbin    sbindingsbin   gsbi      bindings  ndingsbin  
-
-"
 
 let () =
+  let data = load_file filename in
   Printf.eprintf "data length: %d\n" (String.length data);
   let strm = new_lzma_stream() in
 
